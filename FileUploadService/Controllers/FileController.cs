d6320ec6457e7d41897617a4dc9767280d44a2a4
zchutiwat.j <zchutiwat.j@pttdigital.com>
2024-02-16 15:31:07 +0700

    Add Download File

--- FileUploadService/Controllers/FileController.cs
+++ FileUploadService/Controllers/FileController.cs
@@ -7,9 +7,12 @@ using FileUploadService.Models;
 using FileUploadService.Models.ServiceModels;
 using FileUploadService.Models.DBModels;
 using FileUploadService.Services;
+using static FileUploadService.Models.ServiceModels.FileModel;
+using FileUploadService.Context;
 
 namespace FileUploadService.Controllers
 {
+    [Area("api")]
     public class FileController : ControllerBase
     {
         private readonly ILogger<FileController> _logger;
@@ -25,18 +28,112 @@ namespace FileUploadService.Controllers
         [Authorize]
         public async Task<IActionResult> UploadFile([FromForm] FileModel.UploadFile_Req req)
         {
-            int? _accountId = null;
             try
             {
-
-                var accessToken = await HttpContext.GetTokenAsync("access_token");
-                var handler = new JwtSecurityTokenHandler();
-                var token_Payload = handler.ReadJwtToken(accessToken).Payload;
-                _accountId = int.Parse(token_Payload.Sub);
 
                 FileTable res = await _file_Service.UploadFile(req);
 
                 return StatusCode(200, new ResponseModel() { status = 200, data = res });
+            }
+            catch (Exception ex)
+            {
+                return StatusCode(500, new ResponseModel()
+                {
+                    status = 500,
+                    errors = new ExceptionModel()
+                    {
+                        message = ex.Message,
+                    }
+                });
+            }
+        }
+
+        [HttpGet]
+        [Authorize]
+        public async Task<IActionResult> GetInfo(int? fileId)
+        {
+            try
+            {
+                if (!fileId.HasValue)
+                {
+                    return StatusCode(400, new ResponseModel()
+                    {
+                        status = 400,
+                        errors = new ExceptionModel() { message = "FileId must be have value." }
+                    });
+                }
+
+                FileTable res = await _file_Service.GetFileInfo(fileId.Value);
+
+                return StatusCode(200, new ResponseModel() { status = 200, data = res });
+            }
+            catch (Exception ex)
+            {
+
+                return StatusCode(500, new ResponseModel()
+                {
+                    status = 500,
+                    errors = new ExceptionModel()
+                    {
+                        message = ex.Message,
+                    }
+                });
+            }
+        }
+
+        [HttpPost]
+        [Authorize]
+        public async Task<IActionResult> DownloadFile([FromBody] DownloadFile_Req req)
+        {
+            try
+            {
+                if (!req.FileId.HasValue)
+                {
+                    return StatusCode(400, new ResponseModel()
+                    {
+                        status = 400,
+                        errors = new ExceptionModel() { message = "FileId must be have value." }
+                    });
+                }
+
+                DownloadFile_Response _res = await _file_Service.DownloadFile(req.FileId.Value);
+                var res = File(_res.Content, _res.ContentType, Path.GetFileName(_res.FilePath));
+                res.FileDownloadName = _res.FileName;
+
+                return res;
+            }
+            catch (Exception ex)
+            {
+
+                return StatusCode(500, new ResponseModel()
+                {
+                    status = 500,
+                    errors = new ExceptionModel()
+                    {
+                        message = ex.Message,
+                    }
+                });
+            }
+        }
+
+        [HttpPost]
+        [Authorize]
+        public async Task<IActionResult> DeleteFile([FromBody] DeleteFile_Req req)
+        {
+            try
+            {
+                if (!req.FileId.HasValue)
+                {
+                    return StatusCode(400, new ResponseModel()
+                    {
+                        status = 400,
+                        errors = new ExceptionModel() { message = "FileId must be have value." }
+                    });
+                }
+
+                await _file_Service.DeleteFile(req.FileId.Value);
+
+                return StatusCode(200, new ResponseModel() { status = 200, data = null });
             }
             catch (Exception ex)
             {
--- FileUploadService/Services/File_Service.cs
+++ FileUploadService/Services/File_Service.cs
@@ -1,5 +1,6 @@
 ﻿using FileUploadService.Context;
 using FileUploadService.Models.DBModels;
+using Microsoft.AspNetCore.StaticFiles;
 using static FileUploadService.Models.ServiceModels.FileModel;
 
 namespace FileUploadService.Services
@@ -8,8 +9,8 @@ namespace FileUploadService.Services
     {
         public Task<FileTable> UploadFile(UploadFile_Req req);
         public Task<FileTable> GetFileInfo(int fileId);
-        //public Task<DownloadFile_Response> DownloadFile(int fileId);
-        //public Task DeleteFile(int fileId);
+        public Task<DownloadFile_Response> DownloadFile(int fileId);
+        public Task DeleteFile(int fileId);
     }
     public class File_Service : IFile_Service
     {
@@ -25,7 +26,7 @@ namespace FileUploadService.Services
                 string filename_Gen = GenerateUniqueFileName();
                 filename = filename_Gen + extension;
 
-                string filepath = Path.Combine(Directory.GetCurrentDirectory(), "..\\Upload\\");
+                string filepath = Path.Combine(Directory.GetCurrentDirectory(), "Upload\\");
 
                 if (!Directory.Exists(filepath))
                 {
@@ -76,6 +77,62 @@ namespace FileUploadService.Services
             }
 
             return res;
+        }
+
+        public async Task<DownloadFile_Response> DownloadFile(int fileId)
+        {
+            DownloadFile_Response res = new DownloadFile_Response();
+            using (var _coreContext = new CoreContext())
+            {
+                FileTable? file = _coreContext.FileTables.FirstOrDefault(x => x.FileId == fileId && x.IsDeleted == false);
+                if (file == null) throw new Exception("File Not Found");
+
+                string filepath = file.Path;
+
+                var provider = new FileExtensionContentTypeProvider();
+                if (!provider.TryGetContentType(filepath, out var contentType))
+                {
+                    contentType = "application/octet-stream";
+                }
+
+                res.ContentType = contentType;
+                res.FileName = file.Name;
+                res.Content = await System.IO.File.ReadAllBytesAsync(filepath);
+                res.FilePath = filepath;
+            }
+
+            return res;
+        }
+
+        public async Task DeleteFile(int fileId)
+        {
+            try
+            {
+                using (var _coreContext = new CoreContext())
+                {
+                    FileTable? file = _coreContext.FileTables.FirstOrDefault(x => x.FileId == fileId && x.IsDeleted == false);
+                    if (file == null) throw new Exception("File Not Found");
+
+                    string filepath = Path.Combine(Directory.GetCurrentDirectory(), file.Path);
+
+                    // Check if file exists with its full path
+                    if (File.Exists(filepath))
+                    {
+                        // If file found, delete it
+                        File.Delete(Path.Combine(filepath));
+                    }
+                    else throw new Exception("File Not Found in " + filepath);
+
+                    _coreContext.FileTables.Remove(file);
+                    await _coreContext.SaveChangesAsync();
+                }
+
+            }
+            catch (IOException ioExp)
+            {
+                throw new Exception(ioExp.Message);
+            }
+
         }
     }
 }
